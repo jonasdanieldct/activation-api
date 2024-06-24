87c0f5f11455fc4da535a4b5bf1f1e2fe8cb5c48
Jonas Daniel <jonasdanieldct@gmail.com>
2024-06-25 00:02:36 +0800

    Spring Boot Initial Setup

---
+++ src/test/java/com/activation/activation_api/ActivationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activation.activation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
